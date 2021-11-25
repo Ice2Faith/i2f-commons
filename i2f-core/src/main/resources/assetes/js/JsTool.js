69cc5817f84483771ef2bedb79a0933ffc495645
Ice2Faith <Ugex_savelar@163.com>
2021-11-25 21:43:13 +0800

    js painter support self define color base on js color picker

--- i2f-core/src/main/resources/assetes/js/JsTool.js
+++ i2f-core/src/main/resources/assetes/js/JsTool.js
@@ -1662,6 +1662,352 @@ window.$dom = {
 
 }
 
+window.$dlg={
+    pickColorDialog(config,callback=null){
+        let defaultConfig={
+            title:'颜色选择器',
+            width:'480',
+            height:'480',
+            attachId:null,
+            attachDom:null,
+            ctrlHeight:20,
+            background:'white',
+        };
+
+        $obj.copyObj2(config,defaultConfig);
+
+
+        defaultConfig.width=parseInt(defaultConfig.width);
+        defaultConfig.height=parseInt(defaultConfig.height);
+        defaultConfig.ctrlHeight=parseInt(defaultConfig.ctrlHeight);
+
+        let pickColor={
+            hex:'#ff0000',
+            red:255,
+            green:0,
+            blue:128,
+            alpha:255,
+        };
+
+        let virtualId='dlg_color_picker';
+        let ckDom=document.getElementById(virtualId);
+        if(ckDom){
+            ckDom.parentNode.removeChild(ckDom);
+        }
+        let containDom=document.createElement('div');
+        containDom.id=virtualId;
+        document.body.appendChild(containDom);
+
+        containDom.style.position='absolute';
+        containDom.style.left=(window.innerWidth/2)+'px';
+        containDom.style.top=(window.innerHeight/2)+'px';
+        containDom.style.width=defaultConfig.width+'px';
+        containDom.style.height=(defaultConfig.height+defaultConfig.ctrlHeight)+'px';
+        containDom.style.transform='translate(-50%,-50%)';
+        containDom.style.zIndex=2000;
+        containDom.style.display='block';
+        containDom.style.border='solid 1px #eee';
+        containDom.style.padding='5px';
+        containDom.style.borderRadius='5px';
+        containDom.style.background=defaultConfig.background;
+
+        if(defaultConfig.attachDom){
+            let attachDom=defaultConfig.attachDom;
+            containDom.style.left=attachDom.offsetLeft+(attachDom.clientWidth/2);
+            containDom.style.top=attachDom.offsetTop+(attachDom.clientHeight/2);
+        }
+        else if(defaultConfig.attachId && defaultConfig.attachId!=''){
+            let attachDom=document.getElementById(attachId);
+            containDom.style.left=attachDom.offsetLeft+(attachDom.clientWidth/2);
+            containDom.style.top=attachDom.offsetTop+(attachDom.clientHeight/2);
+        }
+
+        let topCtrlDom=document.createElement('div');
+        topCtrlDom.style.width='100%';
+        topCtrlDom.style.height=defaultConfig.ctrlHeight+'px';
+        topCtrlDom.style.borderBottom='solid 1px #eee';
+        containDom.appendChild(topCtrlDom);
+
+        let titleDom=document.createElement('span');
+        topCtrlDom.appendChild(titleDom);
+        titleDom.innerText=defaultConfig.title;
+        titleDom.style.height='100%';
+        titleDom.style.float='left';
+        titleDom.style.marginRight='8px';
+        titleDom.style.fontSize=(defaultConfig.ctrlHeight*0.8)+'px';
+
+        let ctrlDom=document.createElement('span');
+        topCtrlDom.appendChild(ctrlDom);
+        ctrlDom.style.textAlign='center';
+        ctrlDom.innerText='X';
+        ctrlDom.style.height='100%';
+        ctrlDom.style.float='right';
+
+        let okEventHandler=function(event){
+            pickColor.red=Math.max(pickColor.red,0);
+            pickColor.green=Math.max(pickColor.green,0);
+            pickColor.blue=Math.max(pickColor.blue,0);
+            pickColor.alpha=Math.max(pickColor.alpha,0);
+            pickColor.red=Math.min(pickColor.red,255);
+            pickColor.green=Math.min(pickColor.green,255);
+            pickColor.blue=Math.min(pickColor.blue,255);
+            pickColor.alpha=Math.min(pickColor.alpha,255);
+
+            let hr=pickColor.red.toString(16);
+            let hg=pickColor.green.toString(16);
+            let hb=pickColor.blue.toString(16);
+            let ha=pickColor.alpha.toString(16);
+            if(hr.length==1){
+                hr='0'+hr;
+            }
+            if(hg.length==1){
+                hg='0'+hg;
+            }
+            if(hb.length==1){
+                hb='0'+hb;
+            }
+            if(ha.length==1){
+                ha='0'+ha;
+            }
+            pickColor.hex='#'+hr+hg+hb+ha;
+            if(callback){
+                callback(pickColor);
+            }
+        }
+        ctrlDom.addEventListener('click',function (event){
+            containDom.style.display='none';
+            containDom.parentNode.removeChild(containDom);
+            okEventHandler(event);
+        });
+
+
+        let blueCanvasDom=document.createElement('canvas');
+        containDom.appendChild(blueCanvasDom);
+        blueCanvasDom.width=defaultConfig.width;
+        blueCanvasDom.height=defaultConfig.ctrlHeight/2;
+        let blueCanvasState={
+            isDown:false,
+        }
+
+        let blueEventHandler=function (event){
+            if(blueCanvasState.isDown){
+                let posX=(event.offsetX/blueCanvasDom.width)*255;
+                posX=Math.round(posX);
+                pickColor.blue=posX;
+                rerenderArea();
+            }
+        }
+        blueCanvasDom.addEventListener('mousedown',function(event){
+            blueCanvasState.isDown=true;
+        });
+        blueCanvasDom.addEventListener('mousemove',function(event){
+            blueEventHandler(event);
+        });
+        blueCanvasDom.addEventListener('mouseup',function(event){
+            blueEventHandler(event);
+            blueCanvasState.isDown=false;
+        });
+        blueCanvasDom.addEventListener('mouseleave',function(event){
+            blueEventHandler(event);
+            blueCanvasState.isDown=false;
+        });
+
+        if(!blueCanvasDom.getContext){
+            console.error('not support!');
+            return ;
+        }
+
+        let dc2dBlue=blueCanvasDom.getContext('2d');
+        let blueImgData=dc2dBlue.createImageData(blueCanvasDom.width,blueCanvasDom.height);
+        for(let i=0;i<blueCanvasDom.height;i++){
+            let ri=(i/blueCanvasDom.height)*255;
+            ri=Math.round(ri);
+            for(let j=0;j<blueCanvasDom.width;j++){
+                let rj=(j/blueCanvasDom.width)*255;
+                rj=Math.round(rj);
+                let pidx=(i*blueCanvasDom.width+j)*4; //rgba
+
+                let r=blueImgData.data[pidx+0];
+                let g=blueImgData.data[pidx+1];
+                let b=blueImgData.data[pidx+2];
+                let a=blueImgData.data[pidx+3];
+
+                blueImgData.data[pidx+0]=0;
+                blueImgData.data[pidx+1]=0;
+                blueImgData.data[pidx+2]=rj;
+                blueImgData.data[pidx+3]=255;
+
+            }
+        }
+
+        dc2dBlue.putImageData(blueImgData,0,0);
+
+        let alphaCanvasDom=document.createElement('canvas');
+        containDom.appendChild(alphaCanvasDom);
+        alphaCanvasDom.width=defaultConfig.width;
+        alphaCanvasDom.height=defaultConfig.ctrlHeight/2;
+
+        let alphaCanvasState={
+            isDown:false,
+        }
+
+        let alphaEventHandler=function(event){
+            if(alphaCanvasState.isDown){
+                let posX=(event.offsetX/alphaCanvasDom.width)*255;
+                posX=Math.round(posX);
+                pickColor.alpha=posX;
+                rerenderArea();
+            }
+        }
+
+        alphaCanvasDom.addEventListener('mousedown',function(event){
+            alphaCanvasState.isDown=true;
+        });
+        alphaCanvasDom.addEventListener('mousemove',function(event){
+            alphaEventHandler(event);
+        });
+        alphaCanvasDom.addEventListener('mouseup',function(event){
+            alphaEventHandler(event);
+            alphaCanvasState.isDown=false;
+        });
+        alphaCanvasDom.addEventListener('mouseleave',function(event){
+            alphaEventHandler(event);
+            alphaCanvasState.isDown=false;
+        });
+
+        if(!alphaCanvasDom.getContext){
+            console.log('not support!');
+            return ;
+        }
+
+        let dc2dAlpha=alphaCanvasDom.getContext('2d');
+        let alphaImgData=dc2dAlpha.createImageData(alphaCanvasDom.width,alphaCanvasDom.height);
+        for(let i=0;i<alphaCanvasDom.height;i++){
+            let ri=(i/alphaCanvasDom.height)*255;
+            ri=Math.round(ri);
+            for(let j=0;j<alphaCanvasDom.width;j++){
+                let rj=(j/alphaCanvasDom.width)*255;
+                rj=Math.round(rj);
+                let pidx=(i*alphaCanvasDom.width+j)*4; //rgba
+
+                let r=alphaImgData.data[pidx+0];
+                let g=alphaImgData.data[pidx+1];
+                let b=alphaImgData.data[pidx+2];
+                let a=alphaImgData.data[pidx+3];
+
+                alphaImgData.data[pidx+0]=rj;
+                alphaImgData.data[pidx+1]=rj;
+                alphaImgData.data[pidx+2]=rj;
+                alphaImgData.data[pidx+3]=255;
+
+            }
+        }
+
+        dc2dAlpha.putImageData(alphaImgData,0,0);
+
+        let canvasDom=document.createElement('canvas');
+        containDom.appendChild(canvasDom);
+        canvasDom.width=defaultConfig.width;
+        canvasDom.height=defaultConfig.height;
+        canvasDom.style.border='solid 1px #eee';
+
+        if(!canvasDom.getContext){
+            console.error('not support!');
+            return;
+        }
+
+        let dc2d=canvasDom.getContext('2d');
+        /*
+        //原始图像
+        let imgData=dc2d.createImageData(256,256);
+        for(let i=0;i<256;i++){
+          for(let j=0;j<256;j++){
+            let pidx=(i*256+j)*4; //rgba
+
+            let r=imgData.data[pidx+0];
+            let g=imgData.data[pidx+1];
+            let b=imgData.data[pidx+2];
+            let a=imgData.data[pidx+3];
+
+            imgData.data[pidx+0]=i;
+            imgData.data[pidx+1]=j;
+            imgData.data[pidx+2]=0;
+            imgData.data[pidx+3]=180;
+
+          }
+        }
+        */
+
+        let choiceDomState={
+            lastRenderTime:0
+        };
+
+        let rerenderArea=function(){
+            let now=new Date().getTime();
+            if(now-choiceDomState.lastRenderTime<100){
+                console.log('reduce');
+                return ;
+            }
+            choiceDomState.lastRenderTime=now;
+            //支持缩放
+            let imgData=dc2d.createImageData(defaultConfig.width,defaultConfig.height);
+            for(let i=0;i<defaultConfig.height;i++){
+                let ri=(i/defaultConfig.height)*255;
+                ri=Math.round(ri);
+                for(let j=0;j<defaultConfig.width;j++){
+                    let rj=(j/defaultConfig.width)*255;
+                    rj=Math.round(rj);
+                    let pidx=(i*defaultConfig.width+j)*4; //rgba
+
+                    let r=imgData.data[pidx+0];
+                    let g=imgData.data[pidx+1];
+                    let b=imgData.data[pidx+2];
+                    let a=imgData.data[pidx+3];
+
+                    imgData.data[pidx+0]=ri;
+                    imgData.data[pidx+1]=rj;
+                    imgData.data[pidx+2]=pickColor.blue;
+                    imgData.data[pidx+3]=pickColor.alpha;
+
+                }
+            }
+
+            dc2d.putImageData(imgData,0,0);
+        }
+
+        rerenderArea();
+
+        let eventHandler=function(type,event){
+            let posX=(event.offsetX/defaultConfig.width)*255;
+            let posY=(event.offsetY/defaultConfig.height)*255;
+            posX=Math.round(posX);
+            posY=Math.round(posY);
+            pickColor.red=posY;
+            pickColor.green=posX;
+            if(type=='mousedown'){
+
+            }
+            if(type=='mouseup'){
+                okEventHandler(event);
+                containDom.style.display='none';
+                containDom.parentNode.removeChild(containDom);
+            }
+            if(type=='mousemove'){
+
+            }
+        }
+
+        canvasDom.addEventListener('mousemove',function (event){
+            eventHandler('mousemove',event);
+        });
+        canvasDom.addEventListener('mouseup',function(event){
+            eventHandler('mouseup',event);
+        });
+        canvasDom.addEventListener('mousedown',function (event){
+            eventHandler('mousedown',event);
+        });
+    }
+}
 
 
 window.$canvas={
@@ -1686,6 +2032,9 @@ window.$canvas={
       p1.y=minY;
       p2.x=maxX;
       p2.y=maxY;
+    },
+    angle2Radian(angle){
+      return angle/180*Math.PI;
     },
     middlePoint(p1,p2){
       return {
@@ -1805,6 +2154,7 @@ window.$canvas={
     drawRoundRect(dc2d,p1,p2,radius){
         let beginPoint=$canvas.point2d(p1.x,p1.y);
         let endPoint=$canvas.point2d(p2.x,p2.y);
+        $canvas.regularPoint(beginPoint,endPoint);
         let psize=$canvas.sizePoint(beginPoint,endPoint);
         if(!radius){
             radius=Math.min(psize.width,psize.height)*1/5;
@@ -1824,6 +2174,7 @@ window.$canvas={
     drawFillRoundRect(dc2d,p1,p2,radius){
         let beginPoint=$canvas.point2d(p1.x,p1.y);
         let endPoint=$canvas.point2d(p2.x,p2.y);
+        $canvas.regularPoint(beginPoint,endPoint);
         let psize=$canvas.sizePoint(beginPoint,endPoint);
         if(!radius){
             radius=Math.min(psize.width,psize.height)*1/5;
@@ -2180,6 +2531,31 @@ window.$canvas={
 
         }
 
+        if($dlg && $dlg.pickColorDialog){
+            let childSecDom=document.createElement('li');
+            childSecDom.innerText='自定义';
+            childSecDom.style.borderBottom='solid 2px #00ffff88';
+            childSecDom.addEventListener('click',function(event){
+                $dlg.pickColorDialog({
+                    width:256,
+                    height:256,
+                    attachDom: contextMenuDom
+                },function(color){
+                    state.strokeStyle=color.hex;
+                });
+                state.contextMenu.style.display='none';
+            });
+            childSecDom.addEventListener('mouseover',function(event){
+                event.target.style.borderLeft='solid 2px orangered';
+                event.target.style.paddingLeft='4px';
+            });
+            childSecDom.addEventListener('mouseleave',function(event){
+                event.target.style.borderLeft='none';
+                event.target.style.paddingLeft='0';
+            });
+            fstContainDom.appendChild(childSecDom);
+        }
+
         let lineWidthMenuItems=[
             {
                 text:'1px',
@@ -2353,6 +2729,31 @@ window.$canvas={
             fstContainDom.appendChild(childSecDom);
         }
 
+        if($dlg && $dlg.pickColorDialog){
+            let childSecDom=document.createElement('li');
+            childSecDom.innerText='自定义';
+            childSecDom.style.background='#00ffff88';
+            childSecDom.style.color='black';
+            childSecDom.addEventListener('click',function(event){
+                $dlg.pickColorDialog({
+                    width:256,
+                    height:256,
+                    attachDom: contextMenuDom
+                },function(color){
+                    state.fillStyle=color.hex;
+                });
+                state.contextMenu.style.display='none';
+            });
+            childSecDom.addEventListener('mouseover',function(event){
+                event.target.style.borderLeft='solid 2px orangered';
+                event.target.style.paddingLeft='4px';
+            });
+            childSecDom.addEventListener('mouseleave',function(event){
+                event.target.style.borderLeft='none';
+                event.target.style.paddingLeft='0';
+            });
+            fstContainDom.appendChild(childSecDom);
+        }
 
         childFstDom=document.createElement('ul');
         childFstDom.style.listStyle='none';
@@ -2560,9 +2961,11 @@ window.$canvas={
             fontFamily:'黑体',
             fontColor:'silver',
             opacity:0.3,
-            rotate:-30/180*Math.PI,
+            rotate:-30,
         };
         $obj.copyObj2(config,defaultConfig);
+
+        defaultConfig.rotate=$canvas.angle2Radian(defaultConfig.rotate);
 
         let mainDom=document.getElementById(id);
         let dom=document.createElement('canvas');
